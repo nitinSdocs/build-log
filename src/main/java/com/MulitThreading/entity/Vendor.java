5887ac7930034c053d7e9905f4b7048875fb4638
nitin.jaswani <nitin.jaswani@smartdocs.ai>
2025-01-28 16:41:56 +0530

    Changed Vendor Entity Collection name to ThreadPool_vendor

--- src/main/java/com/MulitThreading/entity/Vendor.java
+++ src/main/java/com/MulitThreading/entity/Vendor.java
@@ -7,7 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
-@Document(collection = "vendor")
+@Document(collection = "ThreadPool_vendor")
 @Data
 @AllArgsConstructor
 @RequiredArgsConstructor
